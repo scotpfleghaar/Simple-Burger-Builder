f583ae92fddfe6970f63b96f4e0cc2386e7d412d
Scot <scot.pfleghaar@gmail.com>
2018-07-06 11:34:08 -0500

    Order Summary Model is complete

---
+++ simple-burger-builder/src/components/Burger/OrderSummary/OrderSummary.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import Aux from '../../../hoc/Aux';
+
+const orderSummary = (props) => {
+    const ingredientSummary = Object.keys(props.ingredients)
+        .map(
+            x => {
+                return <li key={x}><span style={{textTransform: 'capitalize'}}>{x}</span>: {props.ingredients[x]}</li>
+            }
+        );
+
+    // <li>Salad: 1</li>
+    return (
+        <Aux>
+            <h3>Your Order</h3>
+            <p>A delicius Burger with the following ingredients:</p>
+            <ul>
+                {ingredientSummary}
+            </ul>
+            <p>Continue to Checkout</p>
+        </Aux>
+    )
+}
+
+export default orderSummary;
