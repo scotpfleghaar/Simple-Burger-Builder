190f0859251f18216d83e2372bb5437913f73ff7
Scot <scot.pfleghaar@gmail.com>
2018-07-06 10:13:22 -0500

    Build Control Component and styles for the component

--- simple-burger-builder/src/components/Burger/Burger.js
+++ simple-burger-builder/src/components/Burger/Burger.js
@@ -4,12 +4,18 @@ import BurgerIngredient from './Burgeringredient/BurgerIngredient';
 
 
 const burger = (props) => {
-    const transformedIngredients = Object.keys(props.ingredients)
+    let transformedIngredients = Object.keys(props.ingredients)
         .map(x => {
             return [...Array(props.ingredients[x])].map(((__, i) => {
                 return <BurgerIngredient key={x + i} type={x}/>
             }));
-        });
+        }).reduce((arr, el) => {
+            return arr.concat(el);
+        }, []);
+
+        if(transformedIngredients.length === 0){
+            transformedIngredients = <p>Please start adding ingredients</p>
+        }
 
     return (
         <div className={classes.Burger}>
