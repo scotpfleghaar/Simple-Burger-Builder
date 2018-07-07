2f5a6e37f37e0f4c1f3e5745d984db056df356f3
Scot <scot.pfleghaar@gmail.com>
2018-07-07 11:58:51 -0500

    Improving Performance

--- simple-burger-builder/src/components/Burger/OrderSummary/OrderSummary.js
+++ simple-burger-builder/src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,29 +1,32 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Aux from '../../../hoc/Aux';
 import Button from '../../UI/Button/Button'
 
-const orderSummary = (props) => {
-    const ingredientSummary = Object.keys(props.ingredients)
+class OrderSummary extends Component {
+    componentWillUpdate() {
+        console.log('[OrderSummary] => componentWillUpdate')
+    }
+    render() {
+        const ingredientSummary = Object.keys(this.props.ingredients)
         .map(
             x => {
-                return <li key={x}><span style={{textTransform: 'capitalize'}}>{x}</span>: {props.ingredients[x]}</li>
+                return <li key={x}><span style={{textTransform: 'capitalize'}}>{x}</span>: {this.props.ingredients[x]}</li>
             }
         );
-
-    // <li>Salad: 1</li>
-    return (
-        <Aux>
+        return (
+            <Aux>
             <h3>Your Order</h3>
             <p>A delicius Burger with the following ingredients:</p>
             <ul>
                 {ingredientSummary}
             </ul>
-            <p><strong>Total Price: ${props.price.toFixed(2)} </strong></p>
+            <p><strong>Total Price: ${this.props.price.toFixed(2)} </strong></p>
             <p>Continue to Checkout</p>
-            <Button buttonType={'Danger'} clicked={props.purchaseCancelled}>CANCEL</Button>
-            <Button buttonType={'Success'} clicked={props.purchaseContinued}>CONTINUE</Button>
+            <Button buttonType={'Danger'} clicked={this.props.purchaseCancelled}>CANCEL</Button>
+            <Button buttonType={'Success'} clicked={this.props.purchaseContinued}>CONTINUE</Button>
         </Aux>
-    )
+        );
+    }
 }
 
-export default orderSummary;
+export default OrderSummary;
--- simple-burger-builder/src/components/UI/Modal/Modal.js
+++ simple-burger-builder/src/components/UI/Modal/Modal.js
@@ -1,20 +1,33 @@
-import React from 'react'
+import React, { Component } from 'react'
 import classes from './Modal.css'
 import Aux from '../../../hoc/Aux'
 import Backdrop from '../Backdrop/Backdrop'
 
-const modal = (props) => (
-    <Aux>
-        <Backdrop clicked={props.modalClosed} show={props.show}/>
-        <div 
-        style={{
-            transform: props.show ? 'translateY(0)' : 'translateY(-100vh)',
-            opacity: props.show ? '1' : '0',
-        }}
-        className={classes.Modal}>
-            {props.children}
-        </div>
-    </Aux>
-)
+class Modal extends Component {
 
-export default modal;
+    shouldComponentUpdate(nextProps, nextState){
+        return nextProps.show !== this.props.show
+    }
+
+    componentWillUpdate() {
+        console.log('[Modal] => componentWillUpdate');
+    }
+
+    render(){
+        return (
+            <Aux>
+                <Backdrop clicked={this.props.modalClosed} show={this.props.show}/>
+                <div 
+                style={{
+                    transform: this.props.show ? 'translateY(0)' : 'translateY(-100vh)',
+                    opacity: this.props.show ? '1' : '0',
+                }}
+                className={classes.Modal}>
+                    {this.props.children}
+                </div>
+            </Aux>
+        );
+    }
+} 
+
+export default Modal;
