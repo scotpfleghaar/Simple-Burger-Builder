ad63e6fc52d1effb0bb1d29a721e90ca41d036d4
Scot <scot.pfleghaar@gmail.com>
2018-07-07 10:20:22 -0500

    Side Drawer slide out implemented

--- simple-burger-builder/src/components/Logo/Logo.js
+++ simple-burger-builder/src/components/Logo/Logo.js
@@ -3,7 +3,9 @@ import logoImg from '../../assets/images/burger-logo.png'
 import classes from './Logo.css'
 
 const logo = (props) => (
-    <div className={classes.Logo} style={{height: props.height}}>
+    <div className={classes.Logo} style={{
+        height: props.height
+    }}>
         <img src={logoImg} alt="Logo Burger"/>
     </div>
 )
