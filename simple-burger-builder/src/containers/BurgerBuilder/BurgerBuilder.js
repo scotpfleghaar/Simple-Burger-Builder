0b30a3139deee62971c4078988397e4788d577ed
Scot <scot.pfleghaar@gmail.com>
2018-07-08 12:08:00 -0500

    handling errors globally

--- simple-burger-builder/src/containers/BurgerBuilder/BurgerBuilder.js
+++ simple-burger-builder/src/containers/BurgerBuilder/BurgerBuilder.js
@@ -6,6 +6,7 @@ import Modal from '../../components/UI/Modal/Modal'
 import OrderSummary from '../../components/Burger/OrderSummary/OrderSummary'
 import axios from '../../axios-oreders'
 import Spinner from '../../components/UI/Spinner/Spinner'
+import withErrorHandler from '../../hoc/withErrorHandler/withErrorHander'
 
 const INGREDIENT_PRICES = {
     salad: 0.5,
@@ -143,4 +144,4 @@ class BurgerBuilder extends Component {
     };
 }
 
-export default BurgerBuilder;
+export default withErrorHandler(BurgerBuilder, axios);
