2945591ddec83f9824e614c5c8de6b9e32877c87
Scot <scot.pfleghaar@gmail.com>
2018-07-10 18:49:37 -0500

    Adding Navlinks

--- simple-burger-builder/src/components/Navigation/NavigationItems/NavigationItems.js
+++ simple-burger-builder/src/components/Navigation/NavigationItems/NavigationItems.js
@@ -4,11 +4,11 @@ import NavigationItem from './NavigationItem/NavigationItem'
 
 const navigationItems = (props) => (
     <ul className={classes.NavigationItems}>
-        <NavigationItem link="/" active>
+        <NavigationItem exact link="/">
             Burger Builder
         </NavigationItem>
-        <NavigationItem link="/">
-            Checkout
+        <NavigationItem link="/orders">
+            Orders
         </NavigationItem>
     </ul>
 )
--- simple-burger-builder/src/containers/Orders/Order/Order.js
+++ simple-burger-builder/src/containers/Orders/Order/Order.js
@@ -5,7 +5,7 @@ const order = (props) => (
     <div className={classes.Order}>
         <p>Ingredients: Salad (1)</p>
         <p>Price:
-            <strong>USD 5.45</strong>
+            <strong> USD 5.45</strong>
         </p>
     </div>
 )
