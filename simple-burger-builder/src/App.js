f6483b6c550bc4bc1390f5e86d4c758e5cd01db9
Scot <scot.pfleghaar@gmail.com>
2018-07-10 16:32:30 -0500

    Building the checkout container

--- simple-burger-builder/src/App.js
+++ simple-burger-builder/src/App.js
@@ -1,16 +1,18 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import Layout from './hoc/Layout/Layout'
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder'
+import CheckoutSummary from './containers/Checkout/Checkout'
 class App extends Component {
-  render() {
-    return (
-      <div>
-        <Layout>
-          <BurgerBuilder/>
-        </Layout>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div>
+                <Layout>
+                    <BurgerBuilder/>
+                    <CheckoutSummary/>
+                </Layout>
+            </div>
+        );
+    }
 }
 
 export default App;
