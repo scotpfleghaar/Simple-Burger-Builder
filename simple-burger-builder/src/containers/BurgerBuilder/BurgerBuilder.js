98caf2d1dcbd4b176e3c73ed304324eec382e940
Scot <scot.pfleghaar@gmail.com>
2018-07-10 17:28:24 -0500

    Passing information in the query params

--- simple-burger-builder/src/containers/BurgerBuilder/BurgerBuilder.js
+++ simple-burger-builder/src/containers/BurgerBuilder/BurgerBuilder.js
@@ -94,15 +94,25 @@ class BurgerBuilder extends Component {
         // ingredients: this.state.ingredients,     price: this.state.totalPrice,
         // customer: {         name: 'Scot',         address: {             street:
         // 'test',             zip: '12345',             country: "USA"         },
-        //   email: 'test@test.com'     },     deliveryMethod: 'fastest' } axios
-        // .post('/orders.json', order)     .then(reponse => {
-        // console.log(reponse)         this.setState({loading: false, purchasing:
-        // false});     })     .catch(error => {         console.error(error.message);
-        //       this.setState({loading: false, purchasing: false});     });
+        // email: 'test@test.com'     },     deliveryMethod: 'fastest' } axios
+        // .post('/orders.json', order)     .then(reponse => { console.log(reponse)
+        // this.setState({loading: false, purchasing: false});     }) .catch(error => {
+        //       console.error(error.message); this.setState({loading: false,
+        // purchasing: false});     });
+        const queryParams = [];
+        for (let i in this.state.ingredients) {
+            queryParams.push(encodeURIComponent(i) + '=' + encodeURIComponent(this.state.ingredients[i]));
+        }
+
+        const queryString = queryParams.join('&');
+
         this
             .props
             .history
-            .push('/checkout');
+            .push({
+                pathname: '/checkout',
+                search: '?' + queryString
+            });
     }
 
     render() {
