f6f9effac774b8b792da90d3544ed3986c8a81ac
Scot <scot.pfleghaar@gmail.com>
2018-07-13 06:42:57 -0500

    Handling user input and two way binding

--- simple-burger-builder/src/containers/Checkout/ContactData/ContactData.js
+++ simple-burger-builder/src/containers/Checkout/ContactData/ContactData.js
@@ -66,6 +66,8 @@ class ContactData extends Component {
         loading: false
     }
 
+
+
     orderHandler = (event) => {
         event.preventDefault();
         this.setState({loading: true})
@@ -99,6 +101,22 @@ class ContactData extends Component {
             });
     }
 
+    inputChangedHandler = (event, inputIdentifyer) => {
+        const updatedOrderForm = {
+            ...this.state.orderForm
+        };
+        const updatedFormElement = {
+            ...updatedOrderForm[inputIdentifyer]
+        };
+
+        updatedFormElement.value = event.target.value;
+
+        updatedOrderForm[inputIdentifyer] = updatedFormElement;
+
+        this.setState({orderForm: updatedOrderForm});
+
+    }
+
     render() {
         const formElements = [];
         for (let key in this.state.orderForm) {
@@ -112,7 +130,8 @@ class ContactData extends Component {
                         key={formElement.id}
                         elementType={formElement.config.elementType}
                         elementConfig={formElement.config.elementConfig}
-                        value={formElement.config.value}/>);
+                        value={formElement.config.value}
+                        changed={(event) => this.inputChangedHandler(event, formElement.id)}/>);
                 })}
                 <Input
                     inputtype='input'
