1b1b5892c23ed70327ab5a56fa9a15a9390fa7cc
Scot <scot.pfleghaar@gmail.com>
2018-07-14 16:15:09 -0500

    adjusting contact data and checkout

--- simple-burger-builder/src/containers/BurgerBuilder/BurgerBuilder.js
+++ simple-burger-builder/src/containers/BurgerBuilder/BurgerBuilder.js
@@ -86,19 +86,19 @@ class BurgerBuilder extends Component {
         // this.setState({loading: false, purchasing: false});     }) .catch(error => {
         //   console.error(error.message); this.setState({loading: false, purchasing:
         // false});     });
-        const queryParams = [];
-        for (let i in this.state.ingredients) {
-            queryParams.push(encodeURIComponent(i) + '=' + encodeURIComponent(this.state.ingredients[i]));
-        }
-        queryParams.push('price=' + this.state.totalPrice);
-        const queryString = queryParams.join('&');
+        // const queryParams = [];
+        // for (let i in this.state.ingredients) {
+        //     queryParams.push(encodeURIComponent(i) + '=' + encodeURIComponent(this.state.ingredients[i]));
+        // }
+        // queryParams.push('price=' + this.state.totalPrice);
+        // const queryString = queryParams.join('&');
 
         this
             .props
             .history
             .push({
                 pathname: '/checkout',
-                search: '?' + queryString
+                // search: '?' + queryString
             });
     }
 
