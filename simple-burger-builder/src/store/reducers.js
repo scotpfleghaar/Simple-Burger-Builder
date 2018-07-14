238d481a25cec76a275c071c4133612071f189e0
Scot <scot.pfleghaar@gmail.com>
2018-07-14 15:11:27 -0500

    installing redux and react-redux

---
+++ simple-burger-builder/src/store/reducers.js
@@ -0,0 +1,12 @@
+import * as actionTypes from './actions'
+
+const intitialState = {
+    ingredients: null,
+    totalPrice: 4,
+};
+
+const reducer = (state = intitialState, actions) => {
+
+}
+
+export default reducer;
