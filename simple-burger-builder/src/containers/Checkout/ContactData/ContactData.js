da57d12d528e2374e79b0cfd7195e1dc304594f5
Scot <scot.pfleghaar@gmail.com>
2018-07-16 17:51:55 -0500

    Connecting contact data contaioner and actions

--- simple-burger-builder/src/containers/Checkout/ContactData/ContactData.js
+++ simple-burger-builder/src/containers/Checkout/ContactData/ContactData.js
@@ -5,6 +5,8 @@ import Spinner from '../../../components/UI/Spinner/Spinner';
 import classes from './ContactData.css';
 import axios from '../../../axios-oreders';
 import Input from '../../../components/UI/Input/Input';
+import withErrorHandler from '../../../hoc/withErrorHandler/withErrorHander'
+import * as actions from '../../../store/actions/index'
 
 class ContactData extends Component {
     state = {
@@ -100,7 +102,7 @@ class ContactData extends Component {
 
     orderHandler = (event) => {
         event.preventDefault();
-        this.setState({loading: true});
+        // this.setState({loading: true});
         const formData = {};
         for (let formElementIdentifier in this.state.orderForm) {
             formData[formElementIdentifier] = this.state.orderForm[formElementIdentifier].value;
@@ -110,6 +112,8 @@ class ContactData extends Component {
             price: this.props.price,
             orderData: formData
         }
+
+        this.props.onOrderBurger(order)
         // axios
         //     .post('/orders.json', order)
         //     .then(response => {
@@ -203,4 +207,8 @@ const mapStateToProps = state => {
 
 }
 
-export default connect(mapStateToProps)(ContactData);
+const mapDispatchToProps = dispatch => {
+    onOrderBurger: (orderData) => dispatch(actions.purchaseBurgerStart(orderData))
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(withErrorHandler(ContactData, axios));
